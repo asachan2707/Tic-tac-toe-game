d2c8a456086e2adc337fd18a33fcdb91a8147b94
Abhinav Sachan <asachan2707>
2019-05-10 13:39:17 +0530

    Tic-tac-toe game initial commit. Game shows following states: 1. Next 2. Winner 3. Draw

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+class Square extends React.Component {
+    render() {
+        return (<button className="square">{this.props.value}</button>);
+    }
+}
